f60df26d33bc67f44dd974ce2f3ccf1cabf8904b
Will Bicks <will@willbicks.com>
2022-01-22 11:32:38 -0500

    create preliminary quiz http handlers

---
+++ application/entry_quiz.go
@@ -0,0 +1,58 @@
+package application
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/willbicks/charisms/service"
+)
+
+// quizTD represents the template data (TD) needed to render the quiz page
+type quizTD struct {
+	Issues    []string
+	Questions []service.QuizQuestion
+}
+
+// quizHandler handles requests to the quizpage (/)
+func (s *CharismsServer) quizHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		err := s.tmpl.ExecuteTemplate(w, "quiz.gohtml", s.Cfg.RootTD.joinPage(quizTD{
+			Questions: s.QuizService.EntryQuestions,
+		}))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			fmt.Println(err)
+		}
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Unable to parse form", http.StatusBadRequest)
+			return
+		}
+
+		var answers map[int]string
+		for id, value := range r.PostForm {
+			id, err := strconv.Atoi(id)
+			if err != nil {
+				http.Error(w, "Invalid form value key", http.StatusBadRequest)
+			}
+			answers[id] = value[0]
+		}
+
+		if s.QuizService.VerifyAnswers(answers) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("answers correct"))
+			return
+		} else {
+			w.WriteHeader(http.StatusForbidden)
+			w.Write([]byte("at least one answer is incorrect"))
+			return
+		}
+
+	default:
+		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+		return
+	}
+
+}
