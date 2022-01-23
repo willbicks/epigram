587461286c036b1ced67a284b91495585f93dff8
Will Bicks <will@willbicks.com>
2022-01-23 18:26:54 -0500

    add return statements to fix nil pointer dereference panic

--- application/auth_middlware.go
+++ application/auth_middlware.go
@@ -1,6 +1,8 @@
 package application
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // interpretSession wraps the request's context with the authenticated user, if they are known.
 // Otherwise, execution passes to the next handler.
@@ -10,17 +12,31 @@ func (s *CharismsServer) interpretSession(next http.Handler) http.Handler {
 		if err != nil {
 			// unable to read cookie (probably not set / no session)
 			next.ServeHTTP(w, r)
+			return
 		}
 
 		u, err := s.UserService.GetUserFromSessionID(r.Context(), c.Value)
 		if err != nil {
 			// session token is invalid
 			next.ServeHTTP(w, r)
+			return
 		}
 
 		ctx := ContextWithUser(r.Context(), u)
-
 		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// requireLoggedIn requires that the request has a valid session which has been translated to a user.
+// If the user is not logged in, they will be redirected to the login page.
+func requireLoggedIn(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u := UserFromContext(r.Context())
+		if u.ID != "" {
+			next.ServeHTTP(w, r)
+		} else {
+			http.Redirect(w, r, "/login", http.StatusFound)
+		}
 	})
 }
 
